f639fe375a30b7bb96173eb151f6ff3b5d67477c
Masahiro Igarashi <igarashi@MacBook-Pro-6.local>
2019-03-16 18:34:39 +0900

    make ORM

--- boards/urls.py
+++ boards/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'boards'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:pk>/', views.board_topics, name='board_topics'),
-    path('<int:pk>/new/', views.new_topic, name='new_topic'),
+    path('<int:board_pk>/', views.board_topics, name='board_topics'),
+    path('<int:board_pk>/new/', views.new_topic, name='new_topic'),
+    path('<int:board_pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('<int:board_pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
 ]
